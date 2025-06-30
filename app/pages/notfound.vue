722cca69160b18c385e9ebc9c9b0af6b26575bc0
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-06-30 21:20:30 +0200

    [ADD] add button comeback to model s on notfound page

--- app/pages/notfound.vue
+++ app/pages/notfound.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="h-screen w-full flex justify-center items-center">
-    <p class="text-[70px] font-InterTight text-black font-extrabold">
-      404 NOT FOUND
-    </p>
+    <div class="flex flex-col">
+      <p
+        class="text-[70px] font-InterTight text-black font-extrabold leading-[70px]"
+      >
+        404 NOT FOUND
+      </p>
+      <a
+        href="/"
+        class="mt-[16px] py-[16px] text-black border-black border-[3px] rounded-[4px] font-InterTight text-center text-[24px] leading-[28px] hover:bg-black hover:text-white"
+        >Model S</a
+      >
+    </div>
   </div>
 </template>
 
